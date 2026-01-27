41fbad9508554e2a09ca04fafc411eda4144b03b
decatile <ohmycatile@gmail.com>
2026-01-27 19:32:05 +0300

    add ability to restart whisper server

--- Program.cs
+++ Program.cs
@@ -28,25 +28,12 @@ namespace ShortWhisper
             };
             trayIcon.MouseClick += (a, b) => { if (b.Button == MouseButtons.Left) new MainForm().Show(); };
             trayIcon.ContextMenuStrip.Items.Add("Settings", null, (a, b) => new SettingsForm().Show());
+            trayIcon.ContextMenuStrip.Items.Add("Restart server", null, (a, b) => WhisperController.Start());
             trayIcon.ContextMenuStrip.Items.Add("Exit", null, (a, b) => Application.Exit());
 
-            Process whisperServer = null;
-            try
-            {
-                whisperServer = Process.Start(new ProcessStartInfo(
-                    Settings.Default.ServerBinaryPath,
-                    $"-m {Settings.Default.ModelPath} --port {Settings.Default.ServerPort}"
-                )
-                {
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                });
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show($"Cannot start whisper server. Perhaps, you need to fill settings. Then save and restart app.\nInner error: {e.Message}");
-            }
+            WhisperController.Start();
 
-            Application.ApplicationExit += (a, b) => whisperServer?.Kill();
+            Application.ApplicationExit += (a, b) => WhisperController.Process?.Kill();
 
             Application.Run();
         }
---
+++ WhisperController.cs
@@ -0,0 +1,30 @@
+﻿using ShortWhisper.Properties;
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ShortWhisper
+{
+    internal class WhisperController
+    {
+        public static Process Process = null;
+
+        public static void Start()
+        {
+            Process?.Kill();
+            Process = null;
+            try
+            {
+                Process = Process.Start(new ProcessStartInfo(
+                        Settings.Default.ServerBinaryPath,
+                        $"-m {Settings.Default.ModelPath} --port {Settings.Default.ServerPort}"
+                )
+                { WindowStyle = ProcessWindowStyle.Hidden });
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Cannot start whisper server. Perhaps, you need to fill settings. Then save and restart server.\nInner error: {e.Message}");
+            }
+        }
+    }
+}
